2d1801a28b260a7dd9b2b9118c0a88fa281e44f3
Kanchu Harish <119840058+22A91A12A2@users.noreply.github.com>
2025-03-10 20:34:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Common in 3 Sorted Arrays/common-in-3-sorted-arrays.py
@@ -0,0 +1,31 @@
+#User function Template for python3
+
+class Solution:
+    def commonElements(self, arr1, arr2, arr3):
+        #Code Here
+        a = set(arr1)
+        b = set(arr2)
+        c = set(arr3)
+        res = a.intersection(b).intersection(c)
+        list= sorted(res)
+        return list if list else [-1]
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        arr = list(map(int, input().split()))
+        brr = list(map(int, input().split()))
+        crr = list(map(int, input().split()))
+        ob = Solution()
+        res = ob.commonElements(arr, brr, crr)
+        if len(res) == 0:
+            print(-1)
+        else:
+            print(" ".join(map(str, res)))
+        print("~")
+# } Driver Code Ends
