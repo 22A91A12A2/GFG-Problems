f22aa88bba1122bd5d4c906566cef31ae2c1facf
Kanchu Harish <119840058+22A91A12A2@users.noreply.github.com>
2025-03-27 23:48:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sum 1 to n Divisors/sum-1-to-n-divisors.py
@@ -0,0 +1,26 @@
+#User function Template for python3
+
+
+class Solution:
+    def sumOfDivisors(self, n):
+    	#code here 
+    	f=0
+        for i in range(1,n+1):
+            f = f + (n//i)*i
+        return f
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        ob = Solution()
+        ans = ob.sumOfDivisors(N)
+        print(ans)
+        print("~")
+
+# } Driver Code Ends
